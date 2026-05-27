9ec2a067d96b69d545579dee2e5d57670a1c2db3
Akkala Sai Mukesh Kumar <126863891+Mukeshdiksha2018@users.noreply.github.com>
2026-05-27 12:25:21 +0530

    config setup
    config setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,13 @@
+import tailwindcss from '@tailwindcss/vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui'],
+  modules: ['@nuxt/ui', '@pinia/nuxt', 'nuxt-tiptap-editor'],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   css: ['~/assets/css/main.css'],
   ui: {
     theme: {
