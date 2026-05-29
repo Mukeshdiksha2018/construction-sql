90d52f778670a29b02f995e4131d05c9668229de
Akkala Sai Mukesh Kumar <126863891+Mukeshdiksha2018@users.noreply.github.com>
2026-05-29 16:07:40 +0530

    UI
    UI

--- app/layouts/main-layout.vue
+++ app/layouts/main-layout.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="flex h-full min-h-0 flex-1 min-w-0 bg-gray-50 dark:bg-gray-900 overflow-hidden">
-    <LayoutSideMenu />
+    <LayoutSideMenu v-if="!hideMainNavigationForNimble" />
 
     <div class="flex min-h-0 min-w-0 flex-1 flex-col overflow-hidden">
-      <LayoutTopBar class="w-full flex-shrink-0" />
+      <LayoutTopBar v-if="!hideMainNavigationForNimble" class="w-full flex-shrink-0" />
 
       <main class="min-h-0 flex-1 p-2 bg-white dark:bg-gray-900 rounded-xl lg:rounded-xl mb-16 lg:mb-2 min-w-0 overflow-x-auto overflow-y-auto">
         <div class="h-min w-full max-w-full min-h-0">
@@ -20,4 +20,8 @@
 import LayoutSideMenu from '~/components/layout/SideMenu.vue'
 import LayoutTopBar from '~/components/layout/TopBar.vue'
 import LayoutMobileBottomNav from '~/components/layout/MobileBottomNav.vue'
+
+const runtimeConfig = useRuntimeConfig()
+const hideMainNavigationForNimble =
+  String(runtimeConfig.public.nimbleIntegrations || '').toLowerCase() === 'true'
 </script>
